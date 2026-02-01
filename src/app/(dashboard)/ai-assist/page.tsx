9cea70d0f12a0a92c630f4546454bbde943774d4
Abhihsek <abhishek.buzzhire@gmail.com>
2026-02-01 16:55:25 +0530

    Added missing routes for new pages

---
+++ src/app/(dashboard)/ai-assist/page.tsx
@@ -0,0 +1,9 @@
+const  AiAssistPage = () => {
+    return (
+        <div>
+        <h1>AI Assistance Page - Work in progress</h1>
+        </div>
+    )
+};
+
+export default AiAssistPage;
---
+++ src/app/(dashboard)/database/page.tsx
@@ -0,0 +1,9 @@
+const  CandidateSearchPage = () => {
+    return (
+        <div>
+        <h1>Candidate Search Page - Work in progress</h1>
+        </div>
+    )
+};
+
+export default CandidateSearchPage;
