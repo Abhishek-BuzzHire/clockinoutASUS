774e0b5c77383ce30fab2af19c6dd55ad0aa0fea
Abhihsek <abhishek.buzzhire@gmail.com>
2026-02-18 13:22:23 +0530

    commented weeklydata

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -55,7 +55,7 @@ const AdminDashboard = () => {
                 <div className='p-4 flex gap-4 flex-col md:flex-row bg-blueLight-50'>
                     <div className="w-full lg:w-2/3 flex flex-col">
                         {/* < Pipeline /> */}
-                        < WeeklyStats />
+                        {/* < WeeklyStats /> */}
                     </div>
                     <div className="w-full lg:w-1/3 flex flex-col gap-4">
                         <CVCount data={todayCVData} loading={loading}/>
