24e5b981be715cae4689db120872df9c871000d4
Abhihsek <abhishek.buzzhire@gmail.com>
2026-03-05 16:01:25 +0530

    forgot to forget route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -189,7 +189,7 @@ export default function LoginPage() {
         {/* Forgot */}
         <div className="text-center mt-1 mb-5">
           <span className="text-[13px] font-medium text-blue-600 hover:text-blue-700 cursor-pointer transition">
-            <Link href="/forgot-password">Forgot Password?</Link>
+            <Link href="/forget-password">Forgot Password?</Link>
           </span>
         </div>
 
